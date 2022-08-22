5d738269de5c7945eb7a3e4c6515e14533b10b33
msolorio <msolorio.github@gmail.com>
2022-08-22 11:12:13 -0700

    Adds notes for async branching with TaskEither

--- src/taskEither2.ts
+++ src/taskEither2.ts
@@ -4,11 +4,18 @@ import * as TE from 'fp-ts/lib/taskEither'
 // import * as E from 'fp-ts/lib/Either'
 
 const char_url = 'http://swapi.dev/api/people/'
+// const species_url = 'http://swapi.dev/api/species/'
 
 
-export const fetchChar = async (idx: number): Promise<{ [key: string]: any }> => {
+// export const fetchChar = async (idx: number): Promise<{ [key: string]: any }> => {
+export const fetchChar = async (idx: number): Promise<unknown> => {
   return (await axios.get(`${char_url}/${idx}`)).data
 }
+
+const fetchFromUrl = async (url: string): Promise<unknown> => {
+  return (await axios.get(url)).data
+}
+
 // export const fetchFirstChar = async (): Promise<unknown> => {
 //   return (await axios.get(`${char_url}/1`)).data
 // }
@@ -23,14 +30,36 @@ export const fetchChar = async (idx: number): Promise<{ [key: string]: any }> =>
 // ;(async () => {
 //   const result = await fetchChar(1)
 // })()
-    
+
+const handleHttpError = () => 'failed to retrieve your data'
+
+const handleC3PO = (charObj: any) => {
+  return pipe(
+    TE.tryCatch(async () => {
+      return {
+        ...charObj,
+        name: 'The true hero of the series',
+        species: await fetchFromUrl(charObj.species[0])
+      }
+    }, err => err), // handleHttpError could also easily go here
+    TE.mapLeft(handleHttpError)
+  )
+}
+
+
+// Async Branching with TaskEither ////////////////////////////////////////////////////
 const getChar = (idx: number) => pipe(
   TE.tryCatch(() => fetchChar(idx), (err) => err),
-  TE.mapLeft(() => 'Failed to retrieve data' )
+  // ^ 1st http call
+  TE.mapLeft(handleHttpError),
+  // ^ returns a left of defined val
+  TE.chain((data: any) => data.name === 'C-3PO' ? handleC3PO(data) : TE.right(data))
+  // ^ optional 2nd http call. returns a right of either form
 )()
 
 ;(async () => {
-  const result = await getChar(2)
+  const result = await getChar(1)
+
+  ''
 })()
 
-''
